6785b1b886b91df6df426fa2978e53806420af8d
gtopal <giorgos.topalid@gmail.com>
2020-12-12 17:46:01 +0200

    Add users tab and extending User model by creating the Profile model

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import HomePageView, SignUpView
+from .views import SignUpView, UserListView
 from django.views.generic.base import TemplateView
 
 
 
+
 urlpatterns = [
+path('users/', UserListView.as_view(), name='user_list'),
 path('users/', include('django.contrib.auth.urls')),
 path('', TemplateView.as_view(template_name='home.html'),name='home'),
 path('signup/', SignUpView.as_view(), name='signup'),
--- web/app/views.py
+++ web/app/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 
-
-class HomePageView(TemplateView):
-    template_name = 'home.html'
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
