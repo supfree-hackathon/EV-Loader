1af1f9d56924e3942c361b168b80aaafa580d081
Alexios Karadimos <karadalex@gmail.com>
2020-12-12 12:13:08 +0200

    INstall rest framework and added simple test-endpoint to add sup points

--- evloader/evloader/urls.py
+++ evloader/evloader/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wallet/', include('wallet.urls')),
+    path('sup-points/', include('wallet.urls')),
     path('admin/', admin.site.urls),
 ]
--- evloader/wallet/views.py
+++ evloader/wallet/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['POST'])
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # user = request.data.get("user_email")
+    # tokens = request.data.get("token_amount")
+    # return Response({
+    #     "user_email": user,
+    #     "token_amount": tokens
+    # })
+    return Response(request.data)
