8f7fddc6fb01833013973df1f8b3fb5b45e72f27
Nick Karadimos <nickarad94@gmail.com>
2020-12-12 12:12:17 +0200

    mobile: changed title

--- mobile/screens/ScanQR.tsx
+++ mobile/screens/ScanQR.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function ScanQR() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Σκανάρετε κωδικό QR</Text>
+      <Text style={styles.title}>Σκανάρετε κωδικό QR ή Barcode</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ScanQR.tsx" />
     </View>
